035509895e388e5bc82d2005896d6c5b23dbeaa0
saul <saul.yeponde@gmail.com>
2022-02-24 14:21:31 +0100

    Configuration files per environments
    Added the production local configurations.
    Updated the project details service.
    Upgraded the app version.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  gameProjectBaseUrl: 'https://static-contents-bucket-ez48ao.s3.eu-west-3.amazonaws.com/games-file/',
+  webProjectBaseUrl: 'http://15.236.131.250:8080/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  gameProjectBaseUrl: 'http://localhost/games-file/',
+  webProjectBaseUrl: 'http://localhost:4200/'
 };
 
 /*
